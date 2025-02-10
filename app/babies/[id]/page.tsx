b66a10ab092772afe96b0ee0cd1562d1c15893ea
urbanjakubdev <jakub.73@hotmail.com>
2025-02-10 20:32:50 +0100

    Update feed time input to use device timezone formatting

--- app/babies/[id]/page.tsx
+++ app/babies/[id]/page.tsx
@@ -112,7 +112,7 @@ export default async function BabyPage({
                      type="datetime-local"
                      name="feedTime"
                      required
-                     defaultValue={todayLocal}
+                     defaultValue={formatInTimeZone(new Date(), getDeviceTimeZone(), 'yyyy-MM-dd HH:mm:ss')}
                      // defaultValue={new Date().toLocaleTimeString()}
                      className="w-full p-3 border border-baby-pink/20 rounded-xl bg-white/50 focus:outline-none focus:ring-2 focus:ring-baby-accent/50"
                   />
